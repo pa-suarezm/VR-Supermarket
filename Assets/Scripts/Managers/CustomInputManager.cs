bf50959a023ca727bdec7b9c6dae760bb1125531
Pablo Suarez <pablo@endlessadventuresinc.com>
2024-08-16 08:14:40 -0500

    Prepared code for release, commented out code meant for debugging

--- Assets/Scripts/Managers/CustomInputManager.cs
+++ Assets/Scripts/Managers/CustomInputManager.cs
@@ -8,32 +8,34 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class CustomInputManager : SingletonBehaviour<CustomInputManager>
 {
-	[SerializeField] private InputActionReference _rightHandPrimaryBtnPress;
+  [SerializeField] private InputActionReference _rightHandPrimaryBtnPress;
 
-	private void Awake()
-	{
-		SingletonInit(this);
-	}
+  private void Awake()
+  {
+    SingletonInit(this);
+  }
 
-	private void Start()
-	{
-		_rightHandPrimaryBtnPress.action.performed += RightHandPrimaryBtnPress;
-	}
+  private void Start()
+  {
+    _rightHandPrimaryBtnPress.action.performed += RightHandPrimaryBtnPress;
+  }
 
-	private void RightHandPrimaryBtnPress(CallbackContext context)
-	{
-		/*if (GameStateManager.Instance.CurrentScene == Task.TASK_00)
+  private void RightHandPrimaryBtnPress(CallbackContext context)
+  {
+    if (GameStateManager.Instance.CurrentScene == Task.TASK_00)
+    {
+      // Only the first Task should be skippable with the A button
+      GameStateManager.Instance.GoToNextScene();
+    }
+    else if (GameStateManager.Instance.CurrentScene.Contains(Task.TASK_SUFFIX_SUPERMARKET))
+    {
+      if (SupermarketTaskManager.Instance != null)
+        SupermarketTaskManager.Instance.EndTask();
+    }
+    /* // For debugging purposes
+    else
 		{
 			GameStateManager.Instance.GoToNextScene();
-		}
-		else */if (GameStateManager.Instance.CurrentScene.Contains(Task.TASK_SUFFIX_SUPERMARKET))
-		{
-			if (SupermarketTaskManager.Instance != null)
-				SupermarketTaskManager.Instance.EndTask();
-		}
-		else
-		{
-			GameStateManager.Instance.GoToNextScene();
-		}
-	}
+		}*/
+  }
 }
